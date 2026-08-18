a59c3a7d4d5c7a6b003175b00ad55f264dc3d0e1
marcelofelixdev <marcelofelix1225@gmail.com>
2026-08-18 02:00:31 -0300

    feat: primeiro classe (Cinema.java)

---
+++ src/main/java/com/app/cineticket/domain/entity/Cinema.java
@@ -0,0 +1,27 @@
+package com.app.cineticket.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_cinema")
+public class Cinema {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 18)
+    private String cnpj;
+
+    @Column(nullable = false, length = 200)
+    private String endereco;
+}
